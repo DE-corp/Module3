a6905011e440e33189e6e3606d5323ab1dd21d02
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-09 22:35:19 +0300

    Задание 3.7.4

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -4,15 +4,29 @@ namespace Module3
 {
     class Program
     {
-        
+        enum DaysofWeek : byte
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
         static void Main(string[] args)
         {
             Console.Write("Введи свое имя: ");
-            string Name = Console.ReadLine();
+            var Name = Console.ReadLine();
             Console.Write("Сколько лет: ");
-            int Age = checked((byte)int.Parse(Console.ReadLine()));
-
-            Console.WriteLine($"Тебя зовут {Name}, тебе {Age}");
+            var Age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.Write("Введи полную дату рождения: ");
+            var BirthDate = Console.ReadLine();
+            Console.Write("Какой по счету день недели твой любимый?: ");
+            var FavoriteWeekDay = (DaysofWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine($"\n\nАНКЕТА ПОЛЬЗОВАТЕЛЯ {Name.ToUpper()}\n\nПроверь все ли данные корректно введены!!!\n");
+            Console.WriteLine($"Имя: {Name}\nДата рождения: {BirthDate}\nПолных лет: {Age}\nЛюбимый день недели: {FavoriteWeekDay}");
             Console.ReadKey();
         }
         
